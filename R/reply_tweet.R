f26825a7df79b507cf5ab349ccc998f8d2ff7fa9
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-06-13 10:12:05 -0300

    Some commenting :)

--- R/reply_tweet.R
+++ R/reply_tweet.R
@@ -10,8 +10,10 @@
 #' @importFrom utils head
 #'
 reply_tweet <- function(tweet) {
+  # Get the first media found.
   img_url <- head(tweet$media_url[tweet$media_type == "photo"], 1)
   text <- tweet$text %>% str_squish()
+  # Check if title provided.
   title <- "ggplotized by @ggplotme"
   if (grepl('with title "', text, ignore.case = TRUE)) {
     title <- str_match(
@@ -19,17 +21,22 @@ reply_tweet <- function(tweet) {
       regex('with title "(.*)"', ignore_case = TRUE)
     )[, 2]
   }
+  # Check if x_points (width) provided.
   x_points <- as.integer(
     str_match(text, regex(" (\\d+) *width", ignore_case = TRUE))[, 2]
   )
+  # Check if y_points (height) provided.
   y_points <- as.integer(
     str_match(text, regex(" (\\d+) *height", ignore_case = TRUE))[, 2]
   )
+  # Check if n_colors provided.
   n_colors <- as.integer(
     str_match(text, regex(" (\\d+) *colors", ignore_case = TRUE))[, 2]
   )
 
+  # Create ggplotized image
   new_img <- to_ggplot(img_url, x_points, y_points, n_colors, title)
+  # and tweet it.
   suppressMessages(try(post_tweet(
     status = random_message(),
     media = new_img$file,
--- R/start_ggplotme.R
+++ R/start_ggplotme.R
@@ -19,30 +19,39 @@ start_ggplotme <- function(
   sleep_secs = 15 * 60,
   rtweet_token_file = NULL
 ) {
+  # If rtweet token provided:
   if (!is.null(rtweet_token_file)) {
+    # then load it by editing the default token in the rtweet env.
     assign(
       "twitter_tokens",
       readRDS(rtweet_token_file),
       envir = rtweet:::.state
     )
   }
+  # Avoid R CMD check warnings.
   created_at <- media_type <- NULL
   message(paste0("user: ", user, "\nsleep_secs: ", sleep_secs))
+  # Forever do:
   while (TRUE) {
+    # Get tweets with my username, newer than from_time, and that contain media.
     mentions <- try({
       search_tweets(user, type = "recent", include_rts = FALSE) %>%
         filter(created_at > from_time) %>%
         filter("photo" %in% media_type)
     })
+    # If there was an error (internet mostly) return an empty data.frame .
     if (inherits(mentions, "try-error")) {
       mentions <- data.frame()
     }
     message(paste0(Sys.time(), " - ", nrow(mentions), " tweets to reply."))
+    # Update from_time.
     from_time <- Sys.time()
     if (nrow(mentions) > 0) {
       from_time <- max(mentions$created_at)
+      # Start replying to tweets.
       apply(mentions, 1, reply_tweet)
     }
+    # Let ggplotme sleep a little bit.
     Sys.sleep(sleep_secs)
   }
 }
